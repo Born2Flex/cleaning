c6ecdc60158b0d36d8a31c2cb3465b616f59ce3e
Danylo Shlapak <geografy2314@gmail.com>
2024-10-16 20:58:33 +0300

    added spring admin server

---
+++ cleaning-admin-server/src/main/java/ua/edu/ukma/cleaningadminserver/CleaningAdminServerApplication.java
@@ -0,0 +1,15 @@
+package ua.edu.ukma.cleaningadminserver;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class CleaningAdminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleaningAdminServerApplication.class, args);
+    }
+
+}
