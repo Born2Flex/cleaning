3e756d2704e54e3fb5086af68b300d7df021d19a
Danylo Shlapak <geografy2314@gmail.com>
2024-10-29 12:23:46 +0200

    Add simple gateway server

---
+++ cleaning-gateway/src/main/java/ua/edu/ukma/cleaninggateway/CleaningGatewayApplication.java
@@ -0,0 +1,13 @@
+package ua.edu.ukma.cleaninggateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleaningGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleaningGatewayApplication.class, args);
+    }
+
+}
