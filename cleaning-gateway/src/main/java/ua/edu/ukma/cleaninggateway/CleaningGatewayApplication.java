54829e68230de8e188a25bf193594881d2fffa36
Danylo Shlapak <geografy2314@gmail.com>
2024-10-30 09:33:39 +0200

    Add eureka server

--- cleaning-gateway/src/main/java/ua/edu/ukma/cleaninggateway/CleaningGatewayApplication.java
+++ cleaning-gateway/src/main/java/ua/edu/ukma/cleaninggateway/CleaningGatewayApplication.java
@@ -2,8 +2,10 @@ package ua.edu.ukma.cleaninggateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CleaningGatewayApplication {
 
     public static void main(String[] args) {
